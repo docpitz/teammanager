5c52b68a19cf9c34669f839eb584d67f8cb7db40
Docpitz <florian.pitz@gute-loesung.de>
2019-06-22 14:02:46 +0200

    save groups corrected

--- app/Group.php
+++ app/Group.php
@@ -17,7 +17,6 @@ class Group extends Model
     ];
 
     public function users() {
-        return $this->belongsToMany('App\User')
-            ->withTimestamps();
+        return $this->belongsToMany('App\User');
     }
 }
