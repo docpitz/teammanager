cd4d45e3da38eaf3aff76959a438567e73ba8826
Docpitz <florian.pitz@gute-loesung.de>
2020-05-16 12:37:40 +0200

    deactivate autocomplete in user

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -21,38 +21,37 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <form method="post" action="{{ route('user.store') }}" autocomplete="off"
-                              enctype="multipart/form-data">
+                        <form method="post" action="{{ route('user.store') }}" autocomplete="off" enctype="multipart/form-data">
                             @csrf
                             <h6 class="heading-small text-muted mb-4">{{ __('Neues Teammitglied') }}</h6>
                             <div class="pl-lg-4">
                                 <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Benutzername') }}</label>
-                                    <input type="text" name="username" id="input-name" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" placeholder="{{ __('Benutzername') }}" value="{{ old('username') }}" required autofocus>
+                                    <input type="text" name="username" id="input-name" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" placeholder="{{ __('Benutzername') }}" value="{{ old('username') }}" required autofocus autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'username'])
                                 </div>
                                 <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Vorname') }}</label>
-                                    <input type="text" name="firstname" id="input-name" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" placeholder="{{ __('Vorname') }}" value="{{ old('firstname') }}" required>
+                                    <input type="text" name="firstname" id="input-name" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" placeholder="{{ __('Vorname') }}" value="{{ old('firstname') }}" required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'firstname'])
                                 </div>
                                 <div class="form-group{{ $errors->has('surname') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Nachname') }}</label>
-                                    <input type="text" name="surname" id="input-name" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" placeholder="{{ __('Nachname') }}" value="{{ old('surname') }}" required>
+                                    <input type="text" name="surname" id="input-name" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" placeholder="{{ __('Nachname') }}" value="{{ old('surname') }}" required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'surname'])
                                 </div>
                                 <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-email">{{ __('E-Mail-Adresse') }}</label>
-                                    <input type="email" name="email" id="input-email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('E-Mail') }}" value="{{ old('email') }}" required>
+                                    <input type="email" name="email" id="input-email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('E-Mail') }}" value="{{ old('email') }}" required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'email'])
                                 </div>
                                 <div class="form-group{{ $errors->has('email_optional') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-email_optional">{{ __('Weitere E-Mail-Adresse') }}</label>
-                                    <input type="email" name="email_optional" id="input-email_optional" class="form-control{{ $errors->has('email_optional') ? ' is-invalid' : '' }}" placeholder="{{ __('Weitere E-Mail-Adresse') }}" value="{{ old('email_optional') }}" >
+                                    <input type="email" name="email_optional" id="input-email_optional" class="form-control{{ $errors->has('email_optional') ? ' is-invalid' : '' }}" placeholder="{{ __('Weitere E-Mail-Adresse') }}" value="{{ old('email_optional') }}" autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'email'])
                                 </div>
@@ -67,26 +66,15 @@
 
                                     @include('alerts.feedback', ['field' => 'role_name'])
                                 </div>
-                                <!--
-                                <div class="form-group{{ $errors->has('photo') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-name">{{ __('Profile photo') }}</label>
-                                    <div class="custom-file">
-                                        <input type="file" name="photo" class="custom-file-input{{ $errors->has('photo') ? ' is-invalid' : '' }}" id="input-picture" accept="image/*">
-                                        <label class="custom-file-label" for="input-picture">{{ __('Select profile photo') }}</label>
-                                    </div>
-
-                                    @include('alerts.feedback', ['field' => 'photo'])
-                                </div>
-                                -->
                                 <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-password">{{ __('Passwort') }}</label>
-                                    <input type="password" name="password" id="input-password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Passwort') }}" value="" required>
+                                    <input type="password" name="password" id="input-password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Passwort') }}" value="" required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'password'])
                                 </div>
                                 <div class="form-group">
                                     <label class="form-control-label" for="input-password-confirmation">{{ __('Passwort wiederholen') }}</label>
-                                    <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control" placeholder="{{ __('Passwort wiederholen') }}" value="" required>
+                                    <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control" placeholder="{{ __('Passwort wiederholen') }}" value="" required autocomplete="off">
                                 </div>
 
                                 <div class="text-center">
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -29,31 +29,31 @@
                             <div class="pl-lg-4">
                                 <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Benutzername') }}</label>
-                                    <input type="text" name="username" id="input-name" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" placeholder="{{ __('Benutzername') }}" value="{{ old('username', $user->username) }}"  required autofocus>
+                                    <input type="text" name="username" id="input-name" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" placeholder="{{ __('Benutzername') }}" value="{{ old('username', $user->username) }}"  required autofocus autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'username'])
                                 </div>
                                 <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Vorname') }}</label>
-                                    <input type="text" name="firstname" id="input-name" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" placeholder="{{ __('Vorname') }}" value="{{ old('firstname', $user->firstname) }}"  required>
+                                    <input type="text" name="firstname" id="input-name" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" placeholder="{{ __('Vorname') }}" value="{{ old('firstname', $user->firstname) }}"  required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'firstname'])
                                 </div>
                                 <div class="form-group{{ $errors->has('surname') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Nachname') }}</label>
-                                    <input type="text" name="surname" id="input-name" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" placeholder="{{ __('Nachname') }}" value="{{ old('surname', $user->surname) }}"  required>
+                                    <input type="text" name="surname" id="input-name" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" placeholder="{{ __('Nachname') }}" value="{{ old('surname', $user->surname) }}"  required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'surname'])
                                 </div>
                                 <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-email">{{ __('E-Mail-Adresse') }}</label>
-                                    <input type="email" name="email" id="input-email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('E-Mail-Adresse') }}" value="{{ old('email', $user->email) }}" required>
+                                    <input type="email" name="email" id="input-email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('E-Mail-Adresse') }}" value="{{ old('email', $user->email) }}" required autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'email'])
                                 </div>
                                 <div class="form-group{{ $errors->has('email_optional') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-email_optional">{{ __('Weitere E-Mail-Adresse') }}</label>
-                                    <input type="email" name="email_optional" id="input-email_optional" class="form-control{{ $errors->has('email_optional') ? ' is-invalid' : '' }}" placeholder="{{ __('Weitere E-Mail-Adresse') }}" value="{{ old('email_optional', $user->email_optional) }}">
+                                    <input type="email" name="email_optional" id="input-email_optional" class="form-control{{ $errors->has('email_optional') ? ' is-invalid' : '' }}" placeholder="{{ __('Weitere E-Mail-Adresse') }}" value="{{ old('email_optional', $user->email_optional) }}" autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'email_optional'])
                                 </div>
@@ -68,26 +68,15 @@
 
                                     @include('alerts.feedback', ['field' => 'role_name'])
                                 </div>
-                                <!-- Vorbereiten für eine spätere Version
-                                <div class="form-group{{ $errors->has('photo') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-name">{{ __('Profil-Foto') }}</label>
-                                    <div class="custom-file">
-                                        <input type="file" name="photo" class="custom-file-input{{ $errors->has('photo') ? ' is-invalid' : '' }}" id="input-picture" accept="image/*">
-                                        <label class="custom-file-label" for="input-picture">{{ __('Profil-Foto') }}</label>
-                                    </div>
-
-                                    @include('alerts.feedback', ['field' => 'photo'])
-                                </div>
-                                -->
                                 <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-password">{{ __('Passwort') }}</label>
-                                    <input type="password" name="password" id="input-password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Passwort') }}" value="">
+                                    <input type="password" name="password" id="input-password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Passwort') }}" value="" autocomplete="off">
 
                                     @include('alerts.feedback', ['field' => 'password'])
                                 </div>
                                 <div class="form-group">
                                     <label class="form-control-label" for="input-password-confirmation">{{ __('Passwort wiederholen') }}</label>
-                                    <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control" placeholder="{{ __('Passwort wiederholen') }}" value="">
+                                    <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control" placeholder="{{ __('Passwort wiederholen') }}" value="" autocomplete="off">
                                 </div>
 
                                 <div class="text-center">
