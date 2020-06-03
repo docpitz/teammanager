3a81d58362b095b32db0d1a9cfecca89e681aa98
Docpitz <florian.pitz@gute-loesung.de>
2020-06-03 15:55:52 +0200

    corrected waitlist validation

--- app/Http/Requests/EventBookingOverviewRequest.php
+++ app/Http/Requests/EventBookingOverviewRequest.php
@@ -27,28 +27,45 @@ class EventBookingOverviewRequest extends FormRequest
     public function rules()
     {
         $max_participant = $this->request->get('max_participant');
-        $waitlist = $this->request->get('Waitlist');
-        $rules = [];
+        $promisedKey = ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Promised)->description;
+        $waitlistKey = ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Waitlist)->description;
+        $waitList = $this->request->get($waitlistKey);
+        $promiseList = $this->request->get($promisedKey);
+        $rules = array($promisedKey => ["array",
+            function ($attribute, $promiseList, $fail) use ($waitList, $max_participant) {
+                $errorMessage = [];
+                if($max_participant > 0 && !empty($promiseList) && count($promiseList) > $max_participant) {
+                    array_push($errorMessage, "Es dürfen maximal ".$max_participant." Mitglieder teilnehmen.");;
+                }
 
-        if($max_participant > 0) {
-            $rules[ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Promised)->description] = 'array|max:'.$max_participant;
-        }
-        if(!empty($waitlist) && count($waitlist) > 0) {
-            if(empty($rules)) {
-                $rules[ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Promised)->description] = 'array|size:'.$max_participant;
-            }
-            else {
-                $rules[ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Promised)->description] = 'array|max:'.$max_participant.'|min:'.$max_participant;
-            }
-        }
+                if(!empty($waitList) && count($waitList) > 0 && $max_participant > 0 ) {
+                    if(empty($promiseList) || (!empty($promiseList) && count($promiseList) < $max_participant)) {
+                        array_push($errorMessage, "Die max. erlaubten Teilnehmer sind noch nicht ausgeschöpft. Es dürfen somit keine Personen in die Warteliste.");
+                    }
+                }
+
+                if(!empty($errorMessage)) {
+                    $fail($errorMessage);
+                }
+            }],
+            $waitlistKey => ["array",
+                function ($attribute, $waitList, $fail) use ($promiseList, $max_participant) {
+                    $errorMessage = [];
+                    if(!empty($waitList) && count($waitList) > 0 && $max_participant > 0 ) {
+                        if(empty($promiseList) || (!empty($promiseList) && count($promiseList) < $max_participant)) {
+                            array_push($errorMessage, "Die max. erlaubten Teilnehmer sind noch nicht ausgeschöpft. Es dürfen somit keine Personen in die Warteliste.");
+                        }
+                    }
+
+                    if($max_participant == 0 && !empty($waitList) && count($waitList) > 0) {
+                        array_push($errorMessage, "Es dürfen unbegrentzt Mitglieder teilnehmen. Es dürfen somit keine Personen in die Warteliste.");
+                    }
+
+                    if(!empty($errorMessage)) {
+                        $fail($errorMessage);
+                    }
+                }]
+            );
         return $rules;
-    }
-
-    public function messages()
-    {
-        return [
-            ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Promised)->description.'.max' => 'Es dürfen maximal :max Mitglieder teilnehmen.',
-            ParticipationStatusEnum::getInstance(ParticipationStatusEnum::Promised)->description.'.min' => 'Die max. erlaubten Teilnehmer sind noch nicht ausgeschöpft. Es dürfen somit keine Personen in die Warteliste.',
-        ];
     }
 }
--- resources/views/alerts/errors.blade.php
+++ resources/views/alerts/errors.blade.php
@@ -1,6 +1,6 @@
 @if (!$errors->isEmpty())
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        @foreach ($errors->all() as $error)
+        @foreach (array_unique($errors->all()) as $error)
             {{ $error }} <br>
         @endforeach
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
