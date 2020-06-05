d71c285897f8811c9fd0412b0bb79311b6f29c55
Docpitz <florian.pitz@gute-loesung.de>
2020-06-05 13:39:47 +0200

    removed unused notifications & search

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -2,20 +2,7 @@
 <nav class="navbar navbar-top navbar-expand border-bottom {{ $navClass ?? 'navbar-dark bg-primary' }}">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Search form -->
-            <form class="navbar-search {{ $searchClass ?? 'navbar-search-light' }} form-inline mr-sm-3" id="navbar-search-main">
-                <div class="form-group mb-0">
-                    <div class="input-group input-group-alternative input-group-merge">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-search"></i></span>
-                        </div>
-                        <input class="form-control" placeholder="{{ __('Search') }}" type="text">
-                    </div>
-                </div>
-                <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </form>
+            @include('layouts.headers.search');
             <!-- Navbar links -->
             <ul class="navbar-nav align-items-center ml-md-auto">
                 <li class="nav-item d-xl-none">
@@ -33,117 +20,7 @@
                         <i class="ni ni-zoom-split-in"></i>
                     </a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="ni ni-bell-55"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-xl dropdown-menu-right py-0 overflow-hidden">
-                        <!-- Dropdown header -->
-                        <div class="px-3 py-3">
-                            <h6 class="text-sm text-muted m-0">You have <strong class="text-primary">13</strong> notifications.</h6>
-                        </div>
-                        <!-- List group -->
-                        <div class="list-group list-group-flush">
-                            <a href="#!" class="list-group-item list-group-item-action">
-                                <div class="row align-items-center">
-                                    <div class="col-auto">
-                                    <!-- Avatar -->
-                                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-1.jpg" class="avatar rounded-circle">
-                                    </div>
-                                    <div class="col ml--2">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div>
-                                                <h4 class="mb-0 text-sm">John Snow</h4>
-                                            </div>
-                                            <div class="text-right text-muted">
-                                                <small>2 hrs ago</small>
-                                            </div>
-                                        </div>
-                                        <p class="text-sm mb-0">Let's meet at Starbucks at 11:30. Wdyt?</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#!" class="list-group-item list-group-item-action">
-                                <div class="row align-items-center">
-                                    <div class="col-auto">
-                                        <!-- Avatar -->
-                                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-2.jpg" class="avatar rounded-circle">
-                                    </div>
-                                    <div class="col ml--2">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div>
-                                                <h4 class="mb-0 text-sm">John Snow</h4>
-                                            </div>
-                                            <div class="text-right text-muted">
-                                                <small>3 hrs ago</small>
-                                            </div>
-                                        </div>
-                                        <p class="text-sm mb-0">A new issue has been reported for Argon.</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#!" class="list-group-item list-group-item-action">
-                                <div class="row align-items-center">
-                                    <div class="col-auto">
-                                        <!-- Avatar -->
-                                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-3.jpg" class="avatar rounded-circle">
-                                    </div>
-                                    <div class="col ml--2">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div>
-                                                <h4 class="mb-0 text-sm">John Snow</h4>
-                                            </div>
-                                            <div class="text-right text-muted">
-                                                <small>5 hrs ago</small>
-                                            </div>
-                                        </div>
-                                        <p class="text-sm mb-0">Your posts have been liked a lot.</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#!" class="list-group-item list-group-item-action">
-                                <div class="row align-items-center">
-                                    <div class="col-auto">
-                                        <!-- Avatar -->
-                                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-4.jpg" class="avatar rounded-circle">
-                                    </div>
-                                    <div class="col ml--2">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div>
-                                                <h4 class="mb-0 text-sm">John Snow</h4>
-                                            </div>
-                                            <div class="text-right text-muted">
-                                                <small>2 hrs ago</small>
-                                            </div>
-                                        </div>
-                                        <p class="text-sm mb-0">Let's meet at Starbucks at 11:30. Wdyt?</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#!" class="list-group-item list-group-item-action">
-                                <div class="row align-items-center">
-                                    <div class="col-auto">
-                                        <!-- Avatar -->
-                                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-5.jpg" class="avatar rounded-circle">
-                                    </div>
-                                    <div class="col ml--2">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div>
-                                                <h4 class="mb-0 text-sm">John Snow</h4>
-                                            </div>
-                                            <div class="text-right text-muted">
-                                                <small>3 hrs ago</small>
-                                            </div>
-                                        </div>
-                                        <p class="text-sm mb-0">A new issue has been reported for Argon.</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <!-- View all -->
-                        <a href="#!" class="dropdown-item text-center text-primary font-weight-bold py-3">View all</a>
-                    </div>
-                </li>
+                @include('layouts.headers.notifications')
             </ul>
             <ul class="navbar-nav align-items-center ml-auto ml-md-0">
                 <li class="nav-item dropdown">
@@ -165,10 +42,12 @@
                             <i class="ni ni-single-02"></i>
                             <span>{{ __('Mein Profil') }}</span>
                         </a>
+                        <!--
                         <a href="#!" class="dropdown-item">
                             <i class="ni ni-settings-gear-65"></i>
                             <span>{{ __('Einstellungen') }}</span>
                         </a>
+                        -->
                         <div class="dropdown-divider"></div>
                         <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
