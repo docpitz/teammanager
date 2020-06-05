2b44b1c74ad8efa1cdb5d5b07b04b7c0964222cc
Docpitz <florian.pitz@gute-loesung.de>
2020-06-05 22:39:41 +0200

    more for dateupdate

--- resources/views/helpers/timeperiod.blade.php
+++ resources/views/helpers/timeperiod.blade.php
@@ -1,5 +1,5 @@
 @if($fromDate->isSameDay($toDate))
-    <span class="text-nowrap">{{Date::parse($fromDate)->format($formatDate)}}</span> <span class="text-nowrap">{{$fromDate->format($formatTime).' - '.$toDate->format($formatTime)}} {{__("Uhr")}}</span>
+    <span class="text-nowrap">{{\Jenssegers\Date\Date::parse($fromDate)->format($formatDate)}}</span> <span class="text-nowrap">{{$fromDate->format($formatTime).' - '.$toDate->format($formatTime)}} {{__("Uhr")}}</span>
 @else
-    <span class="text-nowrap">{{Date::parse($fromDate)->format($formatDate.' '.$formatTime)}} -</span> <span class="text-nowrap">{{Date::parse($toDate)->format($formatDate.' '.$formatTime)}} {{__("Uhr")}}</span>
+    <span class="text-nowrap">{{\Jenssegers\Date\Date::parse($fromDate)->format($formatDate.' '.$formatTime)}} -</span> <span class="text-nowrap">{{\Jenssegers\Date\Date::parse($toDate)->format($formatDate.' '.$formatTime)}} {{__("Uhr")}}</span>
 @endif
