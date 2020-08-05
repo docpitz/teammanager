b680fb9fdc3936e315ba5e10f8d923e23bd9378d
Florian Pitz <florian.pitz@tsvhofolding.de>
2020-08-06 00:03:28 +0200

    corrected isResponsibleByUser

--- app/Event.php
+++ app/Event.php
@@ -226,7 +226,7 @@ class Event extends Model implements Recordable
     }
 
     public function isResponsibleByUser(User $user) {
-        return $this->responsibles()->where('user_id', '=', $user->id);
+        return $this->responsibles()->where('user_id', '=', $user->id)->count() >= 1;
     }
 
     public function hasQuietByUser(User $user) {
